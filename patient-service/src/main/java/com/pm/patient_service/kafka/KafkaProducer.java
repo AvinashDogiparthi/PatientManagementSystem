22a767a15dec28ce5ca0ceda94fbbc8b9d60d231
AvinashDogiparthi <dvsubharao1963@gmail.com>
2025-05-28 23:09:55 +0530

    Kafka : producer configuration changes

---
+++ patient-service/src/main/java/com/pm/patient_service/kafka/KafkaProducer.java
@@ -0,0 +1,37 @@
+package com.pm.patient_service.kafka;
+
+import com.pm.patient_service.model.Patient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+import patient.events.PatientEvent;
+
+@Service
+public class KafkaProducer {
+
+    private final KafkaTemplate<String,byte[]> kafkaTemplate;
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaProducer.class);
+
+    public KafkaProducer(KafkaTemplate<String, byte[]> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendEvent(Patient patient){
+        PatientEvent event = PatientEvent.newBuilder()
+                .setPatientId(patient.getId().toString())
+                .setName(patient.getName())
+                .setEmail(patient.getEmail())
+                .setEventType("PATIENT_CREATED")
+                .build();
+
+        try {
+            kafkaTemplate.send("topic",event.toByteArray());
+            LOGGER.info("Successful in pushing event to kakfa for event : {}",event);
+        } catch (Exception e){
+            LOGGER.info("Error sending PatientCreated event : {}",event);
+        }
+    }
+
+
+}
